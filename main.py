77c634b0a9bc7d9f135b8b77a60c8e64c262540b
Vladiuse <vladiuse@gmail.com>
2021-04-07 22:10:23 +0300

    Одна модель, двигаеться

--- main.py
+++ main.py
@@ -5,13 +5,43 @@ WIDTH = 480
 HEIGHT = 360
 FPS = 30
 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        # pygame.sprite.Sprite.__init__(self)
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.x > WIDTH:
+            self.rect.x = 0
+
+
 pygame.init()
 pygame.mixer.init()  # для звука
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
-
+all_sprites = pygame.sprite.Group()
+# Задаем цвета
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+
+
+
+
+
+player = Player()
+all_sprites.add(player)
 
 # Цикл игры
 running = True
@@ -23,11 +53,12 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-
     # Обновление
+    all_sprites.update()
 
     # Рендеринг
     screen.fill(WHITE)
+    all_sprites.draw(screen)
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
