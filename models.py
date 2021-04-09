f8e987d97eafe5a947636fc2e65f930852c7fe5c
Vladiuse <vladiuse@gmail.com>
2021-04-09 03:12:19 +0300

    refactor, all work

--- models.py
+++ models.py
@@ -11,7 +11,6 @@ MY_GRAY = (50, 50, 50)
 
 
 class BaseElement:
-
     off_color = MY_GRAY
     on_color = GREEN
 
@@ -28,68 +27,23 @@ class BaseElement:
     def off(self):
         self.color = BaseElement.off_color
 
-# class NumberElement(BaseElement):
-#
-#     number_code = {'0': [1, 1, 1, 0, 1, 1, 1],
-#                    '1': [0, 0, 1, 0, 0, 1, 0],
-#                    '2': [1, 0, 1, 1, 1, 0, 1],
-#                    '3': [1, 0, 1, 1, 0, 1, 1],
-#                    '4': [0, 1, 1, 1, 0, 1, 0],
-#                    '5': [1, 1, 0, 1, 0, 1, 1],
-#                    '6': [1, 1, 0, 1, 1, 1, 1],
-#                    '7': [1, 0, 1, 0, 0, 1, 0],
-#                    '8': [1, 1, 1, 1, 1, 1, 1],
-#                    '9': [1, 1, 1, 1, 0, 1, 1],
-#                    }
-#
-#     def __init__(self):
+    def work(self, work):
+        if work:
+            return BaseElement.on_color
+        return BaseElement.off_color
 
 
-
-class NumberBlock(BaseElement):
-
-    number_code = {'0': [1, 1, 1, 0, 1, 1, 1],
-                   '1': [0, 0, 1, 0, 0, 1, 0],
-                   '2': [1, 0, 1, 1, 1, 0, 1],
-                   '3': [1, 0, 1, 1, 0, 1, 1],
-                   '4': [0, 1, 1, 1, 0, 1, 0],
-                   '5': [1, 1, 0, 1, 0, 1, 1],
-                   '6': [1, 1, 0, 1, 1, 1, 1],
-                   '7': [1, 0, 1, 0, 0, 1, 0],
-                   '8': [1, 1, 1, 1, 1, 1, 1],
-                   '9': [1, 1, 1, 1, 0, 1, 1],
-                   }
-    # off_color = MY_GRAY
-    # on_color = GREEN
+class NumberElement(BaseElement):
 
     def __init__(self, screen, start_point, width):
         super().__init__(screen=screen, start_point=start_point)
-        # self.screen = screen
-        # self.start_point = start_point
         self.width = width / 5
         self.height = self.width * 4
         self.percent = 0.25
 
-    def draw_number_block(self, number):
-        x = self.x
-        y = self.y
-        coof = self.width / 20
-        coord_coofs = (
-            (x, y),
-            (x - coof * 11, y + coof * 11),
-            (x + self.height - coof * 9, y + coof * 11),
-            (x, y + self.height + coof * 2),
-            (x - coof * 11, y + self.height + coof * 13),
-            (x + self.height - coof * 9, y + self.height + coof * 13),
-            (x, y + self.height * 2 + coof * 4)
-        )
-        colors = [NumberBlock.on_color if x == 1 else NumberBlock.off_color for x in NumberBlock.number_code[number]]
-        is_horizontals = (True, False, False, True, False, False, True)
-        for (x, y), horizontal, color in zip(coord_coofs, is_horizontals, colors):
-            polygon_coords = self.get_polygon_coords((x, y), horizontal=horizontal)
-            pygame.draw.polygon(self.screen, color, polygon_coords)
-
     def get_polygon_coords(self, start_point, horizontal=False):
+        """Принимает координаты и ориентацию блока,
+        возвражает список с нужными координатами для отрисовки элемента"""
         percent = self.height * self.percent / 2
         polygon_coors = ([0, percent], [self.width / 2, 0], [self.width, percent],
                          [self.width, self.height - percent], [self.width / 2, self.height],
@@ -104,6 +58,47 @@ class NumberBlock(BaseElement):
             point[0] += x
             point[1] += y
         return polygon_coors
+
+    def draw_element(self, work, polygon_coords):
+        color = self.work(work)
+        pygame.draw.polygon(self.screen, color, polygon_coords)
+
+
+class NumberBlock(NumberElement):
+    number_code = {'0': [1, 1, 1, 0, 1, 1, 1],
+                   '1': [0, 0, 1, 0, 0, 1, 0],
+                   '2': [1, 0, 1, 1, 1, 0, 1],
+                   '3': [1, 0, 1, 1, 0, 1, 1],
+                   '4': [0, 1, 1, 1, 0, 1, 0],
+                   '5': [1, 1, 0, 1, 0, 1, 1],
+                   '6': [1, 1, 0, 1, 1, 1, 1],
+                   '7': [1, 0, 1, 0, 0, 1, 0],
+                   '8': [1, 1, 1, 1, 1, 1, 1],
+                   '9': [1, 1, 1, 1, 0, 1, 1],
+                   }
+    is_horizontals = (True, False, False, True, False, False, True)
+
+    def get_coofs_for_block_7(self):
+        """Расчитать стартовую точку для каждого из 7 элеметнов блока"""
+        coof = self.width / 20  # коофициет растояния между элементами
+        coord_coofs = (
+            (self.x, self.y),
+            (self.x - coof * 11, self.y + coof * 11),
+            (self.x + self.height - coof * 9, self.y + coof * 11),
+            (self.x, self.y + self.height + coof * 2),
+            (self.x - coof * 11, self.y + self.height + coof * 13),
+            (self.x + self.height - coof * 9, self.y + self.height + coof * 13),
+            (self.x, self.y + self.height * 2 + coof * 4)
+        )
+        return coord_coofs
+
+    def draw_number_block(self, number: str):
+        """принимает число на вход - отриловывает блок из 7 елеметнов с этим числом"""
+        coord_coofs = self.get_coofs_for_block_7()
+        works = [bool(work) for work in NumberBlock.number_code[number]]
+        for start_point, horizontal, work in zip(coord_coofs, NumberBlock.is_horizontals, works):
+            polygon_coords = self.get_polygon_coords(start_point, horizontal=horizontal)
+            self.draw_element(work, polygon_coords)
 
 
 class Clock:
@@ -124,11 +119,11 @@ class Clock:
         y = self.start_point[1]
         numbers = []
         current_time = self.get_current_time()
-        for _ in range(self.count_numbers):
+        for between_number in range(self.count_numbers):
             number = NumberBlock(screen=self.screen, start_point=(x, y), width=width)
             numbers.append(number)
             x += width * 1.2
-            if _ == 1 or (_ == 3 and self.mili_secs is True):
+            if between_number == 1 or (between_number == 3 and self.mili_secs is True):
                 x += width / 4
                 cpor_color = GREEN
                 if int(current_time[-2]) % 2 == 0:
@@ -160,23 +155,20 @@ class Pixel(BaseElement):
 
     def __init__(self, screen, start_point, size):
         super().__init__(screen=screen, start_point=start_point)
-        # self.screen = screen
-        # self.x = start_point[0]
-        # self.y = start_point[1]
         self.size = size
 
     def draw(self):
         pygame.draw.rect(self.screen, color=self.color,
                          rect=(self.x, self.y, self.size, self.size), width=0)
 
-        new_size = self.size *0.9
-        inside_x = int(self.x + self.size/20)
-        inside_y = int(self.y + self.size/20)
+        new_size = self.size * 0.9
+        inside_x = int(self.x + self.size / 20)
+        inside_y = int(self.y + self.size / 20)
         pygame.draw.rect(self.screen, color=BLACK,
                          rect=(inside_x, inside_y, new_size, new_size), width=0)
 
-        center = self.size *0.7
-        center_x = int(self.x + self.size*0.15)
-        center_y = int(self.y + self.size*0.15)
+        center = self.size * 0.7
+        center_x = int(self.x + self.size * 0.15)
+        center_y = int(self.y + self.size * 0.15)
         pygame.draw.rect(self.screen, color=self.color,
                          rect=(center_x, center_y, center, center), width=0)
