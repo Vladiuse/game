887105537a75966c4f3af85ade655425e073a5df
Vladiuse <vladiuse@gmail.com>
2021-04-24 13:50:55 +0300

    Add second game

--- games/game_test.py
+++ games/game_test.py
@@ -1,25 +1,7 @@
+import random as r
 import time
 
 import pygame
-
-
-class Counter:
-
-    @staticmethod
-    def render_counter_param(n):
-        counter = 0
-
-        def render_counter(func):
-            def surrogate(*args, **kwargs):
-                nonlocal counter
-                counter += 1
-                if counter == n:
-                    counter = 0
-                    return func(*args, **kwargs)
-
-            return surrogate
-
-        return render_counter
 
 
 def render_counter_param(n):
@@ -39,19 +21,18 @@ def render_counter_param(n):
 
 
 class PlayerWalk:
-
     max_bullet_count = 3
 
     def __init__(self):
         self.game_status = True
         self.__game_condition = []
+        self.score = None
+        self.start_time = None
+        self.player = None
+        self.bullets = None
+        self.wall = None
         self.start_game()
-        self.score = 0
-        self.start_time = time.time()
-        self.player = Player((5, 18))
-        self.bullets = []
-        self.wall = Wall((0,0))
-
+        self.game_mode = 'build'
 
     def get_screen_pic(self):
         """Передает экрану (он запрашивает эту функцию)
@@ -63,12 +44,25 @@ class PlayerWalk:
         line = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         while len(self.__game_condition) != 20:
             self.__game_condition.append(line.copy())
+        self.score = 0
+        self.start_time = time.time()
+        self.player = Player((5, 18))
+        self.bullets = []
+        self.wall = Wall()
+        self.game_status = True
+
+    def end_game_check(self):
+        if self.wall._get_top() == self.player.get_position()[0]:
+            self.game_status = False
 
     def run(self):
+        self.end_game_check()
         if self.game_status:
             for bullet in self.bullets:
                 bullet.move()
             self.render_pic()
+        else:
+            self.start_game()
 
     def game_key_controller(self, key):
         if key == pygame.K_UP:
@@ -103,42 +97,34 @@ class PlayerWalk:
         if self.bullets:
             self.check_bullet_in_screen()
             for bullet in self.bullets:
-                y,x = bullet.get_obj()
+                y, x = bullet.get_pos()
                 self.__game_condition[y][x] = 1
-
 
     def check_bullet_in_screen(self):
         """Удаление пуль вышедших за экран"""
         for bullet_id, bullet in enumerate(self.bullets):
-            y, x = bullet.get_obj()
+            y, x = bullet.get_pos()
             if y == -1:
                 self.bullets.pop(bullet_id)
 
     def collision(self):
-        """Уладения блока от пули"""
-        wall = self.wall.get_obj()
         if self.bullets:
             for bullet_id, bullet in enumerate(self.bullets):
-                y, x = bullet.get_obj()
-                if (y, x) in wall:
-                    self.wall.drop_brick((y, x))
+                y, x = bullet.get_pos()
+                if (y, x) in self.wall.get_obj():
+                    if self.game_mode == 'build':
+                        self.wall.add_brick((y + 1, x))
+                    else:
+                        self.wall.drop_brick((y, x))
                     self.bullets.pop(bullet_id)
                     self.score += 1
-
-    # def collision(self):
-    #     if self.bullets:
-    #         for bullet_id, bullet in enumerate(self.bullets):
-    #             y, x = bullet.get_obj()
-    #             if (y, x) in self.wall.get_obj():
-    #                 self.wall.add_brick((y + 1, x))
-    #                 self.bullets.pop(bullet_id)
-    #                 self.score += 1
-
+                if y == - 1:
+                    if self.game_mode == 'build':
+                        self.wall.add_brick((y + 1, x))
 
     def draw_wall(self):
-        for y,x in self.wall.get_obj():
+        for y, x in self.wall.get_obj():
             self.__game_condition[y][x] = 1
-
 
 
 class Player:
@@ -165,23 +151,53 @@ class Player:
     def get_position(self):
         return self.y, self.x
 
-class Wall:
 
-    def __init__(self, start_point):
-        self.x = start_point[1]
-        self.y = start_point[0]
-        self.obj = self.__init_wall()
+class Wall:
+    counter_for_new_line = 90
+
+    def __init__(self, start_line_count=0):
+        self.obj = []
+        self.__init_wall(start_line_count)
+        self.counter_for_new_line = Wall.counter_for_new_line
+
+    def __init_wall(self, start_line_count):
+        for y in range(start_line_count):
+            self.__add_line(y)
+
+    def __add_line(self, line_y_pos):
+        y = line_y_pos
+        line = []
+        for x in range(10):
+            pos = (y, x)
+            if r.randint(0, 1):
+                line.append(pos)
+        if len(line) == 10:
+            line.pop(r.randint(0,9))
+        if line:
+            self.obj.extend(line)
+        else:
+            self.__add_line(line_y_pos)
+
+    def _get_top(self):
+        top = 0
+        for y, x in self.obj:
+            if y > top:
+                top = y
+        return top
 
     def get_obj(self):
+        self.auto_line_adder()
+        self._check_line()
         return self.obj
 
-    def __init_wall(self):
-        wall = []
-        for y in range(3):
-            for x in range(10):
-                pos = (y, x)
-                wall.append(pos)
-        return wall
+    def auto_line_adder(self):
+        self.counter_for_new_line -= 1
+        print(self.counter_for_new_line)
+        if self.counter_for_new_line == 0:
+            print('NEW LINE', self.counter_for_new_line)
+            self.counter_for_new_line = Wall.counter_for_new_line
+            self.__move_lines(line_y_pos=-1, direction=1)
+            self.__add_line(line_y_pos=0)
 
     def drop_brick(self, brick_pos):
         self.obj.remove(brick_pos)
@@ -189,10 +205,35 @@ class Wall:
     def add_brick(self, brick_pos):
         self.obj.append(brick_pos)
 
+    def _check_line(self):
+        """Проверяет нет ли заполненных строк"""
+        lines = set([y for y, x in self.obj])
+        for line in lines:
+            line_counter = 0
+            for y, x in self.obj:
+                if y == line:
+                    line_counter += 1
+            if line_counter == 10:
+                self._del_line_and_down_rest(line)
 
+    def _del_line_and_down_rest(self, line_y_pos):
+        self.obj = list(filter(lambda pos: pos[0] != line_y_pos, self.obj))
+        self.__move_lines(line_y_pos, direction=-1)
+
+    def __move_lines(self, line_y_pos, direction=1):
+        """смещает блоки ниже указаной"""
+        new_obj = []
+        for y, x in self.obj:
+            if y > line_y_pos:
+                new_brick = (y + direction, x)
+                new_obj.append(new_brick)
+            else:
+                new_obj.append((y, x))
+        self.obj = new_obj
 
 
 class Bullet:
+
 
     def __init__(self, start_point, direction=None):
         self.x = start_point[1]
@@ -200,15 +241,20 @@ class Bullet:
         self.direction = 'UP' if direction is None else direction
         self.counter = 0
         self.counter = 15
+        self.obj = [[self.y, self.x]]
+
 
     def __str__(self):
-        # return str(self.y) + ' - ' + str(self.x)
         return f'Bullet: y:{self.y} x:{self.x}'
 
-    # @Counter.render_counter_param(30)
+
+    @render_counter_param(15)
     def move(self):
         if self.direction == 'UP':
             self.y -= 1
 
-    def get_obj(self):
+    def get_pos(self):
         return self.y, self.x
+
+    def get_obj(self):
+        return self.obj
